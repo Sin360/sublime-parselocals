e0a2f404939138043a272fccbbae6e857a62fcd4
Stéphane Leroux <gsp_nope@hotmail.com>
2013-07-25 15:58:02 +0200

    Added detection of variables and parameters

--- sublime-parselocals.py
+++ sublime-parselocals.py
@@ -1,16 +1,19 @@
 import sublime, sublime_plugin, re
 
 class ParselocalsCommand(sublime_plugin.TextCommand):
-	
-	active_class = 0
-	active_line = 0
-	active_method = 0
-	datas = []
+
+	def init(self):
+		self.active_class = 0
+		self.active_line = 0
+		self.active_method = 0
+		self.datas = []
 
 	def output(self, message):
 		print(message)
 
 	def run(self, view):
+		# we initialize global variables
+		self.init()
 		sels = self.view.sel()
 		# loop through selections
 		for sel in sels:
@@ -18,29 +21,8 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			# parse each line
 			for line in text.split('\n'):
 				self.active_line = self.active_line + 1
-				self.parse(line)
+				self.parse(line.strip())
 		self.report()
-
-	def report(self):
-		classes = 0
-		procs = 0
-		funcs = 0
-		for item in self.datas:
-			itemClassName = item.__class__.__name__
-			if itemClassName == "Class":
-				classes = classes + 1
-				# self.output("@class : " + item.name)
-				# self.output("@parent : " + item.parent)
-				# self.output("@length : " + str(item.endLine - item.beginLine))
-			elif itemClassName == "Procedure":
-				procs = procs + 1
-				# self.output("@proc : " + item.name)
-				# self.output("@length : " + str(item.endLine - item.beginLine))
-			elif itemClassName == "Function":
-				funcs = funcs + 1
-				# self.output("@func : " + item.name)
-				# self.output("@length : " + str(item.endLine - item.beginLine))
-		self.output(((classes == 2) and (procs == 16) and (funcs == 0)))
 
 	def parse(self, line):
 		rule = "^(\&&)|(\*)"
@@ -80,8 +62,8 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			classObj.endLine = self.active_line
 			return
 
-		rule = "(?i)(PROTECTED )?(HIDDEN )?(PROC(?:EDURE)?)\s"
-		match = re.match(rule, line)
+		rule = "(?i)^(PROTECTED )?(HIDDEN )?(PROC(?:EDURE)?)\s"
+		match = re.search(rule, line)
 		if match:
 			# we get rid of procedure declaration
 			string = re.sub(rule, "", line)
@@ -90,7 +72,7 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			# we save begin line number
 			procObj.beginLine = self.active_line
 			# we look for the name of the procedure
-			match = re.match("\w*", string)
+			match = re.match("\w*", string.strip())
 			if match:
 				procObj.name = match.group(0)
 			# we push our procedure object and save the index
@@ -98,8 +80,8 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			self.active_method = len(self.datas) - 1
 			return
 
-		rule = "(?i)(PROTECTED )?(HIDDEN )?(FUNC(?:TION)?)\s"
-		match = re.match(rule, line)
+		rule = "(?i)^(PROTECTED )?(HIDDEN )?(FUNC(?:TION)?)\s"
+		match = re.search(rule, line)
 		if match:
 			# we get rid of function declaration
 			string = re.sub(rule, "", line)
@@ -108,7 +90,7 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			# we save begin line number
 			funcObj.beginLine = self.active_line
 			# we look for the name of the function
-			match = re.match("\w*", string)
+			match = re.match("\w*", string.strip())
 			if match:
 				funcObj.name = match.group(0)
 			# we push our function object and save the index
@@ -116,8 +98,8 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			self.active_method = len(self.datas) - 1
 			return
 
-		rule = "(?i)^ENDPROC$"
-		match = re.match(rule, line)
+		rule = "(?i)^ENDPROC"
+		match = re.search(rule, line)
 		if match:
 			# we get our active procedure object
 			procObj = self.datas[self.active_method]
@@ -125,8 +107,8 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			procObj.endLine = self.active_line
 			return
 
-		rule = "(?i)^ENDFUNC$"
-		match = re.match(rule, line)
+		rule = "(?i)^ENDFUNC"
+		match = re.search(rule, line)
 		if match:
 			# we get our active function object
 			funcObj = self.datas[self.active_method]
@@ -134,21 +116,90 @@ class ParselocalsCommand(sublime_plugin.TextCommand):
 			funcObj.endLine = self.active_line
 			return
 
+		rule = "(?i)^(?:L)?PARAMETERS"
+		match = re.search(rule, line)
+		if match:
+			# we get our active method object
+			methObj = self.datas[self.active_method]
+			# we get rid of parameters declaration
+			string = re.sub(rule, "", line)
+			params = string.strip()
+			# we split each parameter
+			params = string.split(', ')
+			for param in params:
+				methObj.parameters.append(param.strip())
+			return
+
+		rule = "(?i)^LOCAL (?:ARRAY )?"
+		match = re.search(rule, line)
+		if match:
+			# we get our active method object
+			methObj = self.datas[self.active_method]
+			# we get rid of local declaration
+			string = re.sub(rule, "", line)
+			variables = string.strip()
+			# we split each variable
+			variables = string.split(', ')
+			for variable in variables:
+				methObj.variables.append(variable.strip())
+			return
+
+	def report(self):
+		classes = 0
+		procs = 0
+		funcs = 0
+		for item in self.datas:
+			itemClassName = item.__class__.__name__
+			if itemClassName == "Class":
+				classes = classes + 1
+				self.output("@class : " + item.name)
+				self.output("@parent : " + item.parent)
+				self.output("@length : " + str(item.endLine - item.beginLine))
+			elif itemClassName == "Procedure":
+				procs = procs + 1
+				self.output("@proc : " + item.name)
+				self.output("@length : " + str(item.endLine - item.beginLine))
+				self.output("@params : " + str(len(item.parameters)))
+				self.output("@variables : " + str(len(item.variables)))
+				for param in item.parameters:
+					self.output("@param : " + param)
+				for variable in item.variables:
+					self.output("@variable : " + variable)
+			elif itemClassName == "Function":
+				funcs = funcs + 1
+				self.output("@func : " + item.name)
+				self.output("@length : " + str(item.endLine - item.beginLine))
+				self.output("@params : " + str(len(item.parameters)))
+				self.output("@variables : " + str(len(item.variables)))
+				for param in item.parameters:
+					self.output("@param : " + param)
+				for variable in item.variables:
+					self.output("@variable : " + variable)
+		# self.output(str(classes) + " classes")
+		# self.output(str(procs) + " procs")
+		# self.output(str(funcs) + " funcs")
+		# # self.output(((classes == 2) and (procs == 16) and (funcs == 0)))
+
 class Class:
-	name = ""
-	parent = ""
-	beginLine = 0
-	endLine = 0
-	properties = []
+	def __init__(self):
+		self.name = ""
+		self.parent = ""
+		self.beginLine = 0
+		self.endLine = 0
+		self.properties = []
 
 class Procedure:
-	name = ""
-	beginLine = 0
-	endLine = 0
-	parameters = []
+	def __init__(self):
+		self.name = ""
+		self.beginLine = 0
+		self.endLine = 0
+		self.variables = []
+		self.parameters = []
 
 class Function:
-	name = ""
-	beginLine = 0
-	endLine = 0
-	parameters = []
+	def __init__(self):
+		self.name = ""
+		self.beginLine = 0
+		self.endLine = 0
+		self.variables = []
+		self.parameters = []
